550e60e33f44898ddc4e7ad569ab305470b309f3
futuroDevLeo <leonunes07@outlook.com>
2023-10-29 02:05:18 -0300

    feat(ROUTES): import validation middleware and validation schemes

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 
 const categoryRouter = Router();
 
-categoryRouter.get('/categoria');
+categoryRouter.get('/categoria', () => {
+    console.log('ok');
+});
 
 module.exports = categoryRouter;
