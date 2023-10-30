21e2774a8459a210e3bbf24a2c51f984e25d428a
Jorge Muniz <jorge.mfilho6@gmail.com>
2023-10-30 18:30:13 -0300

    feat(LOGIN): implementado o controlador de login do usuário

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,46 @@
-const knex = require('../connections/knex');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { findByEmail } = require("../database/userDatabase");
+
+const login = async (req, res, next) => {
+  try {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+      return res.status(404).json("Todos os campos são obrigatórios");
+    }
+
+    const userFound = await findByEmail(email);
+
+    if (!userFound) {
+      return res.status(404).json("Usuário ou senha inválido");
+    }
+
+    const correctPassword = await bcrypt.compare(senha, userFound.senha);
+
+    if (!correctPassword) {
+      return res.status(400).json("Email e senha não confere");
+    }
+
+    const userTokenData = {
+      id: userFound.id,
+      nome: userFound.nome,
+      email: userFound.email,
+    };
+
+    const token = jwt.sign(userTokenData, process.env.SECRET_KEY, {
+      expiresIn: "8h",
+    });
+
+    const { senha: newPass, ...userData } = userFound;
+
+    return res.status(200).json({
+      user: userData,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
+};
+
+module.exports = login;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,30 +1,37 @@
-const bcrypt = require('bcrypt');
-const { findByEmail, registerNewUserDatabase } = require('../database/userDatabase');
+const bcrypt = require("bcrypt");
+const {
+  findByEmail,
+  registerNewUserDatabase,
+} = require("../database/userDatabase");
 
-const userRegister = async (req, res) => {    
-    try {
-        const {nome, email, senha} = req.body;
+const userRegister = async (req, res) => {
+  try {
+    const { nome, email, senha } = req.body;
 
-        const user = await findByEmail(email);
+    const user = await findByEmail(email);
 
-        if (user) {
-            return res.status(400).json({
-                mensagem: "O e-mail informado já existe."
-            });
-        }
-
-        const cryptographedPassword = await bcrypt.hash(senha, 10);
-
-        const registeredUser = await registerNewUserDatabase(nome, email, cryptographedPassword);
-
-        return res.status(201).json(registeredUser);
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message
-        });
+    if (user) {
+      return res.status(400).json({
+        mensagem: "O e-mail informado já existe.",
+      });
     }
-}
+
+    const cryptographedPassword = await bcrypt.hash(senha, 10);
+
+    const registeredUser = await registerNewUserDatabase(
+      nome,
+      email,
+      cryptographedPassword
+    );
+
+    return res.status(201).json(registeredUser);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
-    userRegister
+  userRegister,
 };
