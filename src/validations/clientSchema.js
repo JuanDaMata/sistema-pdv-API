7aae01a8a1d373f5d00b3f10e3e85dbb6c394128
futuroDevLeo <leonunes07@outlook.com>
2023-11-03 20:48:51 -0300

    feat(VALIDATIONS): novo middleware de validação de schemas e novos schemas de validação para as rotas de produtos e clientes

--- src/validations/clientSchema.js
+++ src/validations/clientSchema.js
@@ -1,11 +1,25 @@
 const yup = require('yup');
 const { pt } = require('yup-locales');
+const { validate } = require('./loginSchema');
 yup.setLocale(pt);
 
-const userDetailSchema = yup.object({
+const clientRegisterSchema = yup.object({
+    nome: yup.string().required(),
+    email: yup.string().email().required(),
+    cpf: yup.string().min(11).max(14).required(),
+    cep: yup.string().min(8).max(9),
+    rua: yup.string(),
+    numero: yup.string(),
+    bairro: yup.string(),
+    cidade: yup.string(),
+    estado: yup.string().min(2)
+});
+
+const clientParamsIdSchema = yup.object({
     id: yup.number().integer().positive().required()
 });
 
 module.exports = {
-    userDetailSchema,
+    clientRegisterSchema,
+    clientParamsIdSchema
 };
