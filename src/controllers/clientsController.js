6aeb0e7a645e0f89d3babf768f3a7c382bb2d5d2
Jorge Muniz <jorge.mfilho6@gmail.com>
2023-11-04 17:33:00 -0300

    feature(editClient): Implemenatado a função editar cliente

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -1,74 +1,110 @@
-const { findClientByCpf, registerNewClientDatabase } = require("../database/clientDatabase");
-const { findByIdWithContext, listAll, findByEmailWithContext } = require("../database/generic");
+const {
+  findClientByCpf,
+  registerNewClientDatabase,
+  editClientWithContext,
+} = require("../database/clientDatabase");
+const {
+  findByIdWithContext,
+  listAll,
+  findByEmailWithContext,
+} = require("../database/generic");
 
 const clientRegister = async (req, res) => {
-    const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
+  const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } =
+    req.body;
 
-    try {
-        const clientEmailAlreadyExists = await findByEmailWithContext('clientes', email);
-        // const emailAlreadyExists = await findByEmailWithContext('usuarios', email);
-        if (clientEmailAlreadyExists
-            // || emailAlreadyExists
-        ) {
-            return res.status(400).json({
-                mensagem: "O e-mail informado já está cadastrado."
-            });
-        }
-
-        const cpfAlreadyExists = await findClientByCpf(cpf);
-
-        if (cpfAlreadyExists) {
-            return res.status(400).json({
-                mensagem: "O CPF informado já está cadastrado."
-            });
-        }
-
-        const client = {
-            nome,
-            email,
-            cpf,
-            cep,
-            rua,
-            numero,
-            bairro,
-            cidade,
-            estado
-        };
-
-        const newClient = await registerNewClientDatabase(client);
-        return res.status(200).json(newClient);
-    } catch (error) {
-        return res.status(500).json({ mensagem: error.message });
+  try {
+    const clientEmailAlreadyExists = await findByEmailWithContext(
+      "clientes",
+      email
+    );
+    // const emailAlreadyExists = await findByEmailWithContext('usuarios', email);
+    if (
+      clientEmailAlreadyExists
+      // || emailAlreadyExists
+    ) {
+      return res.status(400).json({
+        mensagem: "O e-mail informado já está cadastrado.",
+      });
     }
+
+    const cpfAlreadyExists = await findClientByCpf(cpf);
+
+    if (cpfAlreadyExists) {
+      return res.status(400).json({
+        mensagem: "O CPF informado já está cadastrado.",
+      });
+    }
+
+    const client = {
+      nome,
+      email,
+      cpf,
+      cep,
+      rua,
+      numero,
+      bairro,
+      cidade,
+      estado,
+    };
+
+    const newClient = await registerNewClientDatabase(client);
+    return res.status(200).json(newClient);
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
 };
 
 const detailClient = async (req, res) => {
-    const clientId = req.params.id;
+  const clientId = req.params.id;
 
-    if (!clientId) return res.status(400).json({ msg: "O campo id é obrigatório" });
-    if (isNaN(clientId)) return res.status(400).json({ msg: "O campo id deve ser um número válido" });
+  if (!clientId)
+    return res.status(400).json({ msg: "O campo id é obrigatório" });
+  if (isNaN(clientId))
+    return res
+      .status(400)
+      .json({ msg: "O campo id deve ser um número válido" });
 
-    try {
-        const client = await findByIdWithContext("clientes", clientId);
-        if (!client) return res.status(404).json({ mensagem: "Cliente não encontrado" });
-        return res.status(200).json(client);
-    }
-    catch (error) {
-        return res.status(500).json({ mensagem: error.message });
-    }
-}
+  try {
+    const client = await editClientWithContext("clientes", clientId);
+    if (!client)
+      return res.status(404).json({ mensagem: "Cliente não encontrado" });
+    return res.status(200).json(client);
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
+};
 
 const listAllClients = async (req, res) => {
-    try {
-        return res.status(200).json(await listAll("clientes"));
-    }
-    catch (error) {
-        return res.status(500).json({ mensagem: error.message });
-    }
-}
+  try {
+    return res.status(200).json(await listAll("clientes"));
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
+};
+
+const editClient = async (req, res) => {
+  const clientId = req.params.id;
+
+  if (!clientId)
+    return res.status(400).json({ msg: "O campo id é obrigatório" });
+  if (isNaN(clientId))
+    return res
+      .status(400)
+      .json({ msg: "O campo id deve ser um número válido" });
+
+  try {
+    const client = await findByEmailWithContext("clientes", clientId);
+    if (!client)
+      return res.status(400).json({ mensagem: "Cliente não encontrado" });
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
+};
 
 module.exports = {
-    clientRegister,
-    detailClient,
-    listAllClients
-}
+  clientRegister,
+  detailClient,
+  listAllClients,
+  editClient,
+};
--- src/database/clientDatabase.js
+++ src/database/clientDatabase.js
@@ -1,24 +1,40 @@
-const knex = require('../connections/knex');
+const knex = require("../connections/knex");
 
 const findClientByCpf = async (cpf) => {
-    try {
-        const client = await knex('clientes').where({ cpf }).first();
-        return client;
-    } catch (error) {
-        return new Error("Erro de comunicação.");
-    }
+  try {
+    const client = await knex("clientes").where({ cpf }).first();
+    return client;
+  } catch (error) {
+    return new Error("Erro de comunicação.");
+  }
 };
 
 const registerNewClientDatabase = async (client) => {
-    try {
-        const registeredClient = await knex("clientes").insert(client).returning("*");
-        return registeredClient[0];
-    } catch (error) {
-        return new Error("Erro no cadastro do usuário.");
-    }
+  try {
+    const registeredClient = await knex("clientes")
+      .insert(client)
+      .returning("*");
+    return registeredClient[0];
+  } catch (error) {
+    return new Error("Erro no cadastro do usuário.");
+  }
+};
+
+const editClientWithContext = async (context, id) => {
+  try {
+    const user = await knex(context).where({ id }).update({
+      nome,
+      email,
+      cpf,
+    });
+    return user;
+  } catch (error) {
+    return new Error("Erro de comunicação.");
+  }
 };
 
 module.exports = {
-    findClientByCpf,
-    registerNewClientDatabase
-}
+  findClientByCpf,
+  registerNewClientDatabase,
+  editClientWithContext,
+};
