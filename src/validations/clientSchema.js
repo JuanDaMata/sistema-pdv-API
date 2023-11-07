e38e3667f9e853737034cf4ba1d9c7a071344540
futuroDevLeo <leonunes07@outlook.com>
2023-11-07 19:16:42 -0300

    refactor: refactoring clientes database and clientSchema

--- src/validations/clientSchema.js
+++ src/validations/clientSchema.js
@@ -1,18 +1,17 @@
 const yup = require('yup');
 const { pt } = require('yup-locales');
-const { validate } = require('./loginSchema');
 yup.setLocale(pt);
 
 const clientRegisterSchema = yup.object({
     nome: yup.string().required(),
     email: yup.string().email().required(),
-    cpf: yup.string().min(11).max(14).required(),
-    cep: yup.string().min(8).max(9),
+    cpf: yup.string().max(11).required(),
+    cep: yup.string().length(8),
     rua: yup.string(),
     numero: yup.string(),
     bairro: yup.string(),
     cidade: yup.string(),
-    estado: yup.string().min(2)
+    estado: yup.string().length(2)
 });
 
 const clientParamsIdSchema = yup.object({
