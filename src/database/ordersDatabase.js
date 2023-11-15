5ce43ce2c71e8f6223f961a96805338d042dd070
Jorge Muniz <jorge.mfilho6@gmail.com>
2023-11-15 17:54:07 -0300

    fix(listOrders): Correções do arquivo ordersDatabase: - alteração do nome da função listOrders para listingOrders nos arquivos (ordersDatabase e ordersController) - alteração do nome da coluna (obsercacao) da tabela (pedidos); - alteração do (select) da variável (query); - alteração do nome da tabela (pedido_produtos); - alteração do nome da coluna (pedido_id) da tabela (pedido_produtos); - alteração do nome das variáveis no metodo(forEach e push);

--- src/database/ordersDatabase.js
+++ src/database/ordersDatabase.js
@@ -1,11 +1,21 @@
 const knex = require('../connections/knex');
 
-const listOrders = async (cliente_id) => {
+const listingOrders = async (cliente_id) => {
     try {
         let query = knex('pedidos')
-        .select('pedidos.id', 'pedidos.valor_total', 'pedidos.observação', 'pedidos.cliente_id')
-        .leftJoin('produtos_pedidos', 'pedidos.id', 'produtos_pedidos.pedidos_id')
-        .leftJoin('produtos', 'produtos_pedidos.produtos_id', 'produtos.id');
+        .select(
+            'pedidos.id',
+            'pedidos.valor_total',
+            'pedidos.observacao',
+            'pedidos.cliente_id',
+            'pedido_produtos.id as pedidoProduto_id',
+            'pedido_produtos.quantidade_produto',
+            'pedido_produtos.valor_produto',
+            'pedido_produtos.pedido_id',
+            'pedido_produtos.produto_id'
+            )
+        .leftJoin('pedido_produtos', 'pedidos.id', 'pedido_produtos.pedido_id')
+        .leftJoin('produtos', 'pedido_produtos.produto_id', 'produtos.id');
 
         if (cliente_id) {
             query = query.where('pedidos.cliente_id', cliente_id)
@@ -19,32 +29,32 @@ const listOrders = async (cliente_id) => {
             const orderIndex = orders.findIndex(o => o.pedido.id === row.id);
 
             if (orderIndex === -1) {
-                const pedido = {
+                const order = {
                     id: row.id,
                     valor_total: row.valor_total,
                     observacao: row.observacao,
                     cliente_id: row.cliente_id
                 };
 
-                const produto_pedido = {
-                    id: row.produto_pedido_id,
+                const productOrder = {
+                    id: row.pedidoProduto_id,
                     quantidade_produto: row.quantidade_produto,
                     valor_produto: row.valor_produto,
                     pedido_id: row.pedido_id,
                     produto_id: row.produto_id
                 };
 
-                orders.push({ pedido, pedido_produtos: [produto_pedido] });
+                orders.push({ pedido: order, pedido_produtos: [productOrder] });
             } else {
-                const produto_pedido = {
-                    id: row.produto_pedido_id,
+                const productOrder = {
+                    id: row.pedidoProduto_id,
                     quantidade_produto: row.quantidade_produto,
                     valor_produto: row.valor_produto,
                     pedido_id: row.pedido_id,
                     produto_id: row.produto_id
                 };
 
-                pedidos[orderIndex].pedido_produtos.push(produto_pedido);
+                pedidos[orderIndex].pedido_produtos.push(productOrder);
             }
         });
 
@@ -55,6 +65,6 @@ const listOrders = async (cliente_id) => {
 }
 
 module.exports = {
-    listOrders
+    listingOrders
 }
 
