2e153a63bc811e56235398602674cecb4cf27342
futuroDevLeo <leonunes07@outlook.com>
2023-11-16 20:53:42 -0300

    refactor: removed unnecessary comments and libs

--- src/connections/aws-sdk.js
+++ src/connections/aws-sdk.js
@@ -1,15 +1,3 @@
-// const { S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3');
-// const { Upload } = require('@aws-sdk/lib-storage');
-
-// const s3 = new S3Client({
-//     endpoint: process.env.ENDPOINT_S3,
-//     region: process.env.AWS_REGION,
-//     credentials: {
-//         accessKeyId: process.env.KEY_ID,
-//         secretAccessKey: process.env.KEY_APP
-//     }
-// });
-
 const aws = require('aws-sdk');
 
 const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
