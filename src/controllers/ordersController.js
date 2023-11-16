211f54f78f69440d403cf117dd319c2938f26517
Adielson <adielsonsousa@gmail.comgit config --global user.email adielsonsousa@gmail.comgit config --global user.email adielsonsousa@gmail.com>
2023-11-16 18:46:24 -0300

    feat: Criação da pasta templates e implementação do HTML para o envio de e-mail e adicionada as informações de conexão do .env para o envio de emails.

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -1,16 +1,17 @@
-const {listOrders} = require('../database/ordersDatabase')
+const {listOrders} = require('../database/ordersDatabase');
 
 const listOrders = async (req,res) => {
     try {
-        const clinte_id = req.query.cliente_id;
+        const cliente_id = req.query.cliente_id;
 
-        const orders = await listOrders(clinte_id);
+        const orders = await listOrders(cliente_id);
 
         res.status(200).json(orders)
     } catch (error) {
         return res.status(500).json({ mensagem: error.message });
     }
-}
+};
+
 
 module.exports = {
     listOrders
